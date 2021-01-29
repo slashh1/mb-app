7a2c59cab0845ddeb5f0f88a0adde5525e35b292
slashh1 <ejazhussain007@gmail.com>
2021-01-29 15:32:25 +0530

    changes to deploy to heroku

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'an01)lcv@9u)+*bqhn2(#u^(r6*hwl3uw-wfj3-9m^rp_ey2ls'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','localhost','127.0.0.1']
 
 
 # Application definition
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.test import TestCase
+from .models import Post
+class PostModelTest(TestCase):
+
+    def setup(self):
+        Post.objects.create(text='just a text')
+
+    def test_text_content(self):
+        post=Post.objects.get(id=1)
+        expected_object_name=f'{Post.text}'
+        self.assertEqual(expected_object_name,'just a test')
